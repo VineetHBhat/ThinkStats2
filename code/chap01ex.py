985293760af4ca1dcca2bd91fc9351e5e3c8683d
VineetHBhat <vineetbhat01@gmail.com>
2019-09-21 15:10:56 +0530

    Completed Chapter 01 exercises

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,12 +13,36 @@ import sys
 import nsfg
 import thinkstats2
 
+def read_2002_fem_resp(stata_file = '2002FemResp.dct', data_file = '2002FemResp.dat.gz'):
+    fixed_width_object = thinkstats2.ReadStataDct(stata_file)
+    actual_data = fixed_width_object.ReadFixedWidth(data_file, compression='gzip', nrows=None)
+    return actual_data
+
+def cross_validate_pregnum(resp):
+    preg = nsfg.ReadFemPreg()
+    preg_map = nsfg.MakePregMap(preg)
+
+    for index, item in resp.pregnum.items():
+        case_id = resp.caseid[index]
+        indices = preg_map[case_id]
+
+        if len(indices) != item:
+            print(case_id, len(indices), item)
+            return False
+
+    return True
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+    resp = read_2002_fem_resp()
+    print(resp.pregnum.value_counts().sort_index())
+    assert(len(resp) == 7643)
+    assert(resp.pregnum.value_counts()[0] == 2610)
+    assert(cross_validate_pregnum(resp) == True)
+
     print('%s: All tests passed.' % script)
 
 
